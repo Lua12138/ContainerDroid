12c8fb0c355324fb20dbb2d7da1b9edcec3b13d3
local.codex <local@codex.com>
2025-09-10 16:06:55 +0800

    Fix checkPermission uid

--- Bcore/src/main/java/top/niunaijun/blackbox/fake/service/IActivityManagerProxy.java
+++ Bcore/src/main/java/top/niunaijun/blackbox/fake/service/IActivityManagerProxy.java
@@ -19,10 +19,8 @@ import java.util.ArrayList;
 
 import black.android.app.BRActivityManagerNative;
 import black.android.app.BRActivityManagerOreo;
-import black.android.app.BRIActivityManagerContentProviderHolder;
 import black.android.app.BRLoadedApkReceiverDispatcher;
 import black.android.app.BRLoadedApkReceiverDispatcherInnerReceiver;
-import black.android.content.BRContentProviderHolderOreo;
 import black.android.content.BRContentProviderNative;
 import black.android.content.pm.BRUserInfo;
 import black.android.os.BRUserHandle;
@@ -47,11 +45,11 @@ import top.niunaijun.blackbox.proxy.ProxyManifest;
 import top.niunaijun.blackbox.proxy.record.ProxyBroadcastRecord;
 import top.niunaijun.blackbox.utils.MethodParameterUtils;
 import top.niunaijun.blackbox.utils.Reflector;
-import top.niunaijun.blackbox.utils.Slog;
 import top.niunaijun.blackbox.utils.compat.BuildCompat;
 import top.niunaijun.blackbox.utils.compat.ParceledListSliceCompat;
 
 import static android.content.pm.PackageManager.GET_META_DATA;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 /**
  * Created by Milk on 3/30/21.
@@ -236,11 +234,8 @@ public class IActivityManagerProxy extends ClassInvocationStub {
                 Intent proxyIntent = BlackBoxCore.getBActivityManager().bindService(intent,
                         connection == null ? null : connection.asBinder(),
                         resolvedType,
-                        BActivityThread.getUserId());
+                        userId);
                 if (connection != null) {
-                    if (intent.getComponent() == null && resolveInfo != null) {
-                        intent.setComponent(new ComponentName(resolveInfo.serviceInfo.packageName, resolveInfo.serviceInfo.name));
-                    }
                     args[4] = ServiceConnectionDelegate.createProxy(connection, intent);
                 }
                 if (proxyIntent != null) {
@@ -317,7 +312,7 @@ public class IActivityManagerProxy extends ClassInvocationStub {
         @Override
         protected Object hook(Object who, Method method, Object[] args) throws Throwable {
             MethodParameterUtils.replaceFirstAppPkg(args);
-            Intent[] intents = (Intent[]) args[getIntentsIndex(args)];
+            Intent[] intents = (Intent[]) args[getIntentsIndex()];
 
             // todo
             for (Intent intent : intents) {
@@ -326,22 +321,13 @@ public class IActivityManagerProxy extends ClassInvocationStub {
             return method.invoke(who, args);
         }
 
-        private int getIntentsIndex(Object[] args) {
-            for (int i = 0; i < args.length; i++) {
-                if (args[i] instanceof Intent[]) {
-                    return i;
-                }
-            }
+        private int getIntentsIndex() {
             if (BuildCompat.isR()) {
                 return 6;
             } else {
                 return 5;
             }
         }
-    }
-
-    @ProxyMethod("getIntentSenderWithSourceToken")
-    public static class GetIntentSenderWithSourceToken extends GetIntentSender {
     }
 
     @ProxyMethod("getIntentSenderWithFeature")
@@ -489,7 +475,7 @@ public class IActivityManagerProxy extends ClassInvocationStub {
     public static class GrantUriPermission extends MethodHook {
         @Override
         protected Object hook(Object who, Method method, Object[] args) throws Throwable {
-            MethodParameterUtils.replaceLastUserId(args);
+            MethodParameterUtils.replaceLastUid(args);
             return method.invoke(who, args);
         }
     }
@@ -526,12 +512,21 @@ public class IActivityManagerProxy extends ClassInvocationStub {
     public static class checkPermission extends MethodHook {
         @Override
         protected Object hook(Object who, Method method, Object[] args) throws Throwable {
+            MethodParameterUtils.replaceLastUid(args);
             String permission = (String) args[0];
             if (permission.equals(Manifest.permission.ACCOUNT_MANAGER)
                     || permission.equals(Manifest.permission.SEND_SMS)) {
                 return PackageManager.PERMISSION_GRANTED;
             }
             return method.invoke(who, args);
+        }
+    }
+
+    @ProxyMethod("checkUriPermission")
+    public static class checkUriPermission extends MethodHook {
+        @Override
+        protected Object hook(Object who, Method method, Object[] args) throws Throwable {
+            return PERMISSION_GRANTED;
         }
     }
 }
--- app/src/main/java/top/niunaijun/blackboxa/view/apps/AppsFragment.kt
+++ app/src/main/java/top/niunaijun/blackboxa/view/apps/AppsFragment.kt
@@ -96,7 +96,6 @@ class AppsFragment : Fragment() {
                     if (!isMove(point, e)) {
                         popupMenu?.show()
                     }
-                    popupMenu = null
                     point.set(0, 0)
                 }
 
