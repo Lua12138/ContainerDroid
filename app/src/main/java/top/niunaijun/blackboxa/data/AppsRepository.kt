ca497c3072eaee724396f8fbcdfad34dc0e95aed
local.codex <local@codex.com>
2025-09-22 11:39:56 +0800

    Merge pull request #24 from crazyn2/master
    Fix ILocation

--- app/src/main/java/top/niunaijun/blackboxa/data/AppsRepository.kt
+++ app/src/main/java/top/niunaijun/blackboxa/data/AppsRepository.kt
@@ -24,58 +24,65 @@ import java.io.File
 
 class AppsRepository {
 
+    @Volatile
     private var mInstalledList = mutableListOf<AppInfo>()
 
+    @Volatile
+    private var isLoading = true
+
+
     fun previewInstallList() {
-        synchronized(mInstalledList){
-            val installedApplications: List<ApplicationInfo> = getPackageManager().getInstalledApplications(0)
-            val installedList = mutableListOf<AppInfo>()
+        isLoading = true
+        val installedApplications: List<ApplicationInfo> = getPackageManager().getInstalledApplications(0)
+        val installedList = mutableListOf<AppInfo>()
 
-            for (installedApplication in installedApplications) {
-                val file = File(installedApplication.sourceDir)
+        for (installedApplication in installedApplications) {
+            val file = File(installedApplication.sourceDir)
 
-                if ((installedApplication.flags and  ApplicationInfo.FLAG_SYSTEM) != 0) continue
+            if ((installedApplication.flags and  ApplicationInfo.FLAG_SYSTEM) != 0) continue
 
-                if (!AbiUtils.isSupport(file)) continue
+            if (!AbiUtils.isSupport(file)) continue
 
-                val isXpModule = BlackBoxCore.get().isXposedModule(file)
+            val isXpModule = BlackBoxCore.get().isXposedModule(file)
 
-                val info = AppInfo(
+            val info = AppInfo(
                     installedApplication.loadLabel(getPackageManager()).toString(),
                     installedApplication.loadIcon(getPackageManager()),
                     installedApplication.packageName,
                     installedApplication.sourceDir,
                     isXpModule
-                )
-                installedList.add(info)
-            }
-            this.mInstalledList.clear()
-            this.mInstalledList.addAll(installedList)
+            )
+            installedList.add(info)
         }
 
-
+        this.mInstalledList = installedList
+        isLoading = false
     }
 
     fun getInstalledAppList(previewingLiveData: MutableLiveData<Boolean>, appsLiveData: MutableLiveData<List<AppInfo>>) {
-        previewingLiveData.postValue(true)
-        synchronized(mInstalledList) {
+        previewingLiveData.postValue(isLoading)
+        if (isLoading) {
+            Thread.sleep(500)
+            getInstalledAppList(previewingLiveData, appsLiveData)
+        } else {
+            previewingLiveData.postValue(isLoading)
             appsLiveData.postValue(ArrayList(mInstalledList))
-            previewingLiveData.postValue(false)
         }
-
     }
 
     fun getInstalledModuleList(previewingLiveData: MutableLiveData<Boolean>, appsLiveData: MutableLiveData<List<AppInfo>>) {
 
-        previewingLiveData.postValue(true)
-        synchronized(mInstalledList) {
+        previewingLiveData.postValue(isLoading)
+        if (isLoading) {
+            Thread.sleep(500)
+            getInstalledModuleList(previewingLiveData, appsLiveData)
+        } else {
+            previewingLiveData.postValue(isLoading)
             val moduleList = mInstalledList.filter {
                 it.isXpModule
             }
             appsLiveData.postValue(moduleList)
-            previewingLiveData.postValue(false)
         }
-
     }
 
 
